93ab4b9420e8793a993db2d1dab5514c7d0abf40
Yunjeong Choi <shineanl04@gmail.com>
2023-02-06 20:39:49 +0900

    [Feat] Project Setting

---
+++ src/main/java/com/podong/icanread/IcanreadApplication.java
@@ -0,0 +1,13 @@
+package com.podong.icanread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcanreadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IcanreadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/podong/icanread/IcanreadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.podong.icanread;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IcanreadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
