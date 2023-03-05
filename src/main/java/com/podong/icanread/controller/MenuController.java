b451aba138ee9ac57f7228c2140e030c15a6066a
Yunjeong Choi <shineanl04@gmail.com>
2023-03-05 16:15:25 +0900

    [Feat] Implement menu list API logic between FE and BE (#10)
    - The image is received from FE, and BE sends the reorganized menu list.

--- src/main/java/com/podong/icanread/controller/MenuController.java
+++ src/main/java/com/podong/icanread/controller/MenuController.java
@@ -6,8 +6,12 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.json.simple.parser.ParseException;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -17,13 +21,19 @@ public class MenuController {
 
     // 메뉴 이름으로 메뉴 조회 테스트용 API
     @GetMapping("/api/v1/menu")
-    public MenuDto findByName(@RequestParam("name") String name) throws ParseException {
+    public MenuDto findByName(@RequestParam("name") String name) {
         return menuService.findByName(name);
     }
 
     // Naver Search API 결과 테스트
     @GetMapping("/api/v1/naver")
-    public MenuDto naverSearch(@RequestParam("name") String name) throws ParseException {
+    public MenuDto naverSearch(@RequestParam("name") String name) {
         return menuService.searchByNaver(name);
+    }
+
+    // 이미지 request body로 뒀을 때, 재구성된 메뉴 리스트 조회 테스트 API (FE <-> BE OK)
+    @PostMapping("/api/v1/menu/list")
+    public List<MenuDto> reorganizedMenuList(@RequestParam("file") MultipartFile file) {
+        return menuService.extractedTextListFromML();
     }
 }
--- src/main/java/com/podong/icanread/service/menu/MenuService.java
+++ src/main/java/com/podong/icanread/service/menu/MenuService.java
@@ -10,7 +10,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 @Slf4j
@@ -24,7 +26,7 @@ public class MenuService {
     @Value("${naver.secret}")
     private String naverClientSecret;
 
-    public MenuDto findByName(String name) throws ParseException {
+    public MenuDto findByName(String name) {
         Optional<Menu> entity = menuRepository.findByName(name);
         if (entity.isPresent()){ // 메뉴가 DB에 존재하는 경우
             return new MenuDto(entity.get());
@@ -46,7 +48,7 @@ public class MenuService {
     }
 
     // Naver Encyclopedia Search API 연동
-    public MenuDto searchByNaver(String name) throws ParseException {
+    public MenuDto searchByNaver(String name) {
         NaverClient naverClient = new NaverClient(name, naverClientId, naverClientSecret);
         return saveData(name, naverClient.getMeaning(), naverClient.getImageURL());
     }
@@ -60,5 +62,23 @@ public class MenuService {
                 .build();
         menuRepository.save(entity);
         return new MenuDto(entity);
+    }
+
+    // ML에서 텍스트 리스트 받아오는 로직 (수정 예정)
+    public List<MenuDto> extractedTextListFromML() {
+        String[] data = {"에스프레소", "아메리카노", "카페 모카", "카페 라떼", "카푸치노", "캐러멜 마키아토", "민트 초콜릿",
+                "에스프레소", "바닐라 라떼", "차가운 음료", "오렌지 주스", "애플 주스", "아이스 라떼", "아이스 모카", "아이스 티",
+                "디저트", "레드 벨벳 케이크", "마카롱", "크루아상", "치즈 케이크", "애플 파이", "피칸 파이", "초콜릿 케이크",
+                "민트 컵케이크", "샌드위치", "달걀 & 햄", "오몰렛", "햄 & 치즈", "햄버거", "치즈 버거"};
+        return makeMenuList(new ArrayList<>(Arrays.asList(data)));
+    }
+
+    // ML에서 텍스트 리스트 받아오면 뜻풀이, 이미지 추가한 메뉴판 새로 만들기
+    public List<MenuDto> makeMenuList(ArrayList<String> data) {
+        ArrayList<MenuDto> restructuredList = new ArrayList<>();
+        for (String menuName : data){
+            restructuredList.add(findByName(menuName));
+        }
+        return restructuredList;
     }
 }
